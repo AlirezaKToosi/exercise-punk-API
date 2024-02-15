33211c4f56b5b7b4d6c43159b81dfb80d6161695
Alireza KafshdarToosi <a.k.toosi@gmail.com>
2024-02-15 13:23:55 +0100

    View1 Done

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ async function fetchAndRenderBeers() {
     const randomBeerCard = document.getElementById("beerCard");
     const randomBeerName = document.getElementById("beerName");
     const randomBeerPhoto = document.getElementById("beerPhoto");
-    const randomBeerInfo = document.getElementById("beerInfo")
-    const randomBeerButton = document.getElementById("moreInfoButton");
+    const randomBeerInfo = document.getElementById("beerInfo");
+    const randomBeerLink = document.getElementById("moreInfoLink");
     const response = await fetch("https://api.punkapi.com/v2/beers/random");
     const beers = await response.json();
     beers.forEach((beer) => {
       console.log(beer);
       randomBeerName.innerHTML = `<h2>${beer.name}</h2>`;
-      randomBeerPhoto.innerHTML = `<img src="${beer.image_url}" alt="${beer.name}" style="max-width: 10%; max-height: 10%;">`;
+      randomBeerPhoto.innerHTML = `<img src="${beer.image_url}" alt="${beer.name}">`;
       randomBeerInfo.innerHTML = `<p>${beer.description}</p>`;
-      randomBeerButton.innerHTML = `<button onclick="showBeerDetails(${beer.id})">See more</button>`;
+      randomBeerLink.innerHTML = `<a href="#" onclick="showBeerDetails(${beer.id})" id="moreInfoLink">See more info ... </a>`;
     });
   } catch (error) {
     console.error("Error fetching pokemons:", error);
